ad863bea5ecee7bc753eae0634724831ee3bb5e2
Megha Komarraju <47645860+Megha-Komarraju@users.noreply.github.com>
2021-01-07 11:22:32 -0500

    Add files via upload

---
+++ data_collection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  7 10:33:41 2021
+
+@author: saime
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path="C:/Users/saime/Documents/Kaggle/survey_Project-2020/chromedriver"
+df=gs.get_jobs('data scientist',1000, False,path,15)
+
+df
